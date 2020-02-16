0e952bdd5923b79e4e3683f2220339ca958adf96
Eduardojvr <eduardojvr10@gmail.com>
2020-02-16 20:48:39 -0300

    Configurações iniciais da api rest

---
+++ juniors/src/main/java/com/juniors/rest/RESTCategoria.java
@@ -0,0 +1,35 @@
+package com.juniors.rest;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/categoria")
+public class RESTCategoria {
+
+	@Context
+	private HttpServletRequest request;
+
+	@Context
+	private HttpServletResponse response;
+
+	protected void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	@GET
+	@Path("/todasCategorias")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response get() {
+		return Response.ok().entity("Loja juniors!").build();
+	}
+
+}
